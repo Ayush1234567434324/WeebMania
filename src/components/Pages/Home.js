106176014d950edb4b2044f6e3fc7eb3b4beaf9a
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-10-19 04:57:42 +0530

    first commit

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Footer from "../Footer/footer";
+
 
 import { Mangapage } from "./manga card/manga-page";
 import Videorender from "./video/videorender";
