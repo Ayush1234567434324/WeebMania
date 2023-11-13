abe0a2983556e3a450b492e2145927a9638867cf
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-11-13 14:05:34 +0530

    uou

--- src/components/Pages/manga-pdf/mangaPDF.js
+++ src/components/Pages/manga-pdf/mangaPDF.js
@@ -65,7 +65,7 @@ console.log(stateFromLink)
 
     <div>
 
-         <div style={{position:'absolute',top:'10rem',right:'2rem',zIndex:10}}>
+         <div style={{position:'absolute',top:'10rem',right:'2rem',zIndex:100}}>
           <img height='40px' src='fullscreen.svg' onClick={handle.enter} class="hoverable"/>
       
       </div>
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -39,8 +39,8 @@ function NavBar() {
     <>
       <nav  className={`navbar ${visible ? 'visible' : 'hidden'}`}>
         <div className="nav-container">
-          <div  className="nav-logo" style={{zIndex:'-1200'}} >
-          <div style={{ display: "flex"}}>
+          <div  className="nav-logo"  >
+          <div style={{ display: "flex" , position:'relative'}}>
   <NavLink exact to="/"  ><img className="vegeta-logo" src="https://i.imgur.com/hCtqhTz.png" style={{ height: "8rem", width: "auto",marginTop:"7.8rem" }} /></NavLink>
   <img className="weeb-logo" src="https://i.imgur.com/rb1dKK8.png" style={{ height: "20rem", width: "auto",marginTop:"4rem",opacity:closeClick?"0":"1" }} />
 </div>
